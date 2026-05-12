b3787d98d55cf879fff85054a3293aee0430532c
Nithin <nithinmanit@gmail.com>
2026-05-12 12:06:53 +0530

    Standard Approach.

--- Arrays/189. Rotate Array.py
+++ Arrays/189. Rotate Array.py
@@ -1,8 +1,11 @@
-#With slicing (Python)
+# Standard Approach
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         k = k%len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
